b62ac0e2e04308e3586971b33df9f7a0c2845ea6
QiangZiBro <qiangzibro@gmail.com>
2020-08-07 21:07:43 +0800

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,44 +1,34 @@
 #/bin/bash
 #--------------------------------------------------
-# Build and Initiation script
+# Build and Initiation helper script
 #--------------------------------------------------
-
-#COMMAND="source $( realpath  $(dirname $0)/bin/spd.sh)"
-#
-#if ! grep -Fxq "$COMMAND" ~/.zshrc
-#then
-#    # not found
-#    echo $COMMAND >> ~/.zshrc
-#fi
-
-# config
-#if test "$(uname)" = "Darwin"
-#then
-#	# Mac installation branch
-#    echo You are mac
-#	
-#elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
-#then
-#	# Linux installation branch
-#    echo You are linux
-#    # sudo cp ./ss.service /etc/systemd/system/
-#	
-#fi
 
 if [ "$1" = "build" ]; then
     docker build -t ss:common .
 elif [ "$1" = "rebuild" ];then
     ./bin/spd_service stop 
     docker build -t ss:common .
+
 elif [ "$1" = "all" ]; then
     docker build -t ss:common .
     ./bin/spd_service stop
     ./bin/spd_service start
     ./bin/spd_service exec zsh
+
 elif [ "$1" = "exec" ]; then
     ./bin/spd_service start
     ./bin/spd_service exec zsh
 elif [ "$1" = "reexec" ]; then
     ./bin/spd_service stop
     ./bootstrap.sh exec zsh
+
+elif [ "$1" = "start" ]; then
+    ./bin/spd_service start
+elif [ "$1" = "stop" ]; then
+    ./bin/spd_service stop
+
+elif [ "$1" = "clean" ]; then
+    docker stop $(docker ps -a | grep "Exited" | awk '{print $1 }') #停止容器
+    docker rm $(docker ps -a | grep "Exited" | awk '{print $1 }') #删除容器
+    docker rmi $(docker images | grep "none" | awk '{print $3}') #删除镜像
 fi
