70497a8056b4d74389c2d92f71e6c698cc186c29
QiangZiBro <qiangzibro@gmail.com>
2020-08-08 21:15:36 +0800

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -38,7 +38,7 @@ elif [ "$1" = "update" ]; then
     git push origin HEAD
 
     cd "$root"
-    git pull origin master
+    git pull origin HEAD
     git submodule foreach --recursive git pull origin master
     git add -A && git commit -m 'update '
     git push origin HEAD
